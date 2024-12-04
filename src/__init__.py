e28edf4bbf9f9a0d989970aff5964f5a1f23a2e7
Alexis Daugé <alexis.dauge2004@gmail.com>
2024-12-04 13:49:21 +0000

    ci(pylint): resolve errors

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,36 @@
+"""
+Sample plugin for Binary Ninja that demonstrates how to run a task in the background.
+"""
+
 from binaryninja.plugin import BackgroundTaskThread, PluginCommand
 
 class SampleInBackground(BackgroundTaskThread):
+    """
+    A class to execute a background task within Binary Ninja.
+    """
+
     def __init__(self, bv, msg):
+        """
+        Initialize the background task with the given BinaryView and message.
+        """
         BackgroundTaskThread.__init__(self, msg, True)
         self.bv = bv
 
     def run(self):
+        """
+        Method to execute the background task.
+        """
         pass
 
 def sample_in_background(bv):
+    """
+    Function to start the background task in Binary Ninja.
+    """
     bg_task = SampleInBackground(bv, "Executing sample plugin")
     bg_task.start()
 
-PluginCommand.register("Sample Plugin", "This plugin demonstrates a basic Binary Ninja plugin structure.", sample_in_background)
+PluginCommand.register(
+    "Sample Plugin",
+    "This plugin demonstrates a basic Binary Ninja plugin structure.",
+    sample_in_background
+)
